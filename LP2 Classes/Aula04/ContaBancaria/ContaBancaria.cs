f8033adf082a45c2e1879a5d3dd8a88dedd7e10b
geloodev <angeloholandini11@gmail.com>
2022-03-13 11:46:57 -0300

    add Aula 4

---
+++ LP2 Classes/Aula04/ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01 {
+    internal class ContaBancaria {
+       
+        private double saldoAtual;
+        private string textoErro;
+
+        //CONSTRUTORES
+        public ContaBancaria() { //construtor padrão
+
+            saldoAtual = 100.00;
+            textoErro = "Sucesso.";
+        }
+
+        //METODOS
+        public double saldo() {
+            return saldoAtual;
+        }
+        public string mensagemErro() {
+            return textoErro;
+        }
+        public bool deposito(double valor) { //METODO COM PARAMETROS
+            if (valor >= 50) {
+                descontaTarifa();
+                saldoAtual += valor;
+                return true;
+            }
+            else {
+                textoErro = String.Format("Depósito inválido ({0:C})! Valor mínimo permitido: R$50.00", valor);
+                return false;
+            }
+        }
+        public bool saque(double valor) { //METODO COM PARAMETROS
+            if (valor <= saldoAtual) {
+                descontaTarifa();
+                saldoAtual -= valor;
+                return true;
+            } 
+            else {
+                textoErro = String.Format("Saque inválido ({0:C})! O saque não pode ser maior que o Saldo Atual.", valor);
+                return false;
+            }
+        }
+        public void descontaTarifa() {
+            saldoAtual -= 0.10;
+        }
+    } 
+}
---
+++ LP2 Classes/Aula04/ContaBancaria/ex01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            ContaBancaria c1 = new ContaBancaria();
+
+            // c1.deposito(20);
+            c1.saque(20);
+
+            Console.WriteLine("Mensagem: {0}", c1.mensagemErro());
+            Console.WriteLine("Saldo: {0:C}", c1.saldo());
+        }
+    }
+}
